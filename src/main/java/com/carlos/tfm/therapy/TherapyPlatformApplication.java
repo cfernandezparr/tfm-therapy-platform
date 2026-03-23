b9d269469eb7462f6c358c0cfc11cf3612137b43
Carlos Fernandez Parra <Carlos.Parra@nfoque.local>
2026-03-23 11:51:58 +0100

    Initial commit

---
+++ src/main/java/com/carlos/tfm/therapy/TherapyPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.carlos.tfm.therapy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TherapyPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TherapyPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlos/tfm/therapy/TherapyPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlos.tfm.therapy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TherapyPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
